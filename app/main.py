8e1e2277c08e8f3a6d30c0df3d0882d86ae7e0ad
Arseniy <cenz1217@gmail.com>
2025-09-12 03:28:15 +0500

    added rewrite endpoint

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import JSONResponse
 from openai import AsyncOpenAI
 
 from app.llm_pipelines import BuildMapPipeline
+from app.llm_pipelines.edit_map.pipeline import EditMapPipeline
 from app.settings import settings
 
 from .logic import rewrite_roadmap_with_prompt
@@ -19,6 +20,10 @@ client = AsyncOpenAI(
 
 build_map_pipeline = BuildMapPipeline(
     client=client, model=settings.model_name, model_lite=settings.model_name_lite
+)
+
+edit_map_pipeline = EditMapPipeline(
+    client=client, model=settings.model_name
 )
 
 app = FastAPI(
@@ -53,10 +58,12 @@ async def roadmap_from_files(
 
 
 @app.post('/roadmap/rewrite', response_model=Roadmap, summary='Переписать роадмап по промпту')
-async def roadmap_rewrite(payload: RewriteRequest):
+async def roadmap_rewrite(files: List[UploadFile] = File(..., description='До 5 файлов, ≤5 МБ каждый'), payload: RewriteRequest):
     try:
-        updated = rewrite_roadmap_with_prompt(payload.roadmap, payload.prompt)
-        return JSONResponse(content=updated.model_dump(), status_code=status.HTTP_200_OK)
+        await validate_files(files)
+        material = await extract_text_blobs_to_dict(files)
+        updated_knowledge_map = edit_map_pipeline.edit(material, payload.knowledge_map, payload.user_query)
+        return JSONResponse(content=updated_knowledge_map.model_dump(), status_code=status.HTTP_200_OK)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f'Не удалось переписать роадмап: {e}')
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel
 
+from app.llm_pipelines.models import KnowledgeMap
 
-class RewriteRequest(BaseModel): ...
+
+class RewriteRequest(BaseModel):
+    knowledge_map: KnowledgeMap
+    user_query: str
